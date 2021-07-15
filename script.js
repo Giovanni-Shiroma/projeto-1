4aaec7dc924a404c1ff8a9e5cd12ce21f05c397c
giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-07-15 20:42:23 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
-//  function validar() {
-//     var mercadoria = formulario.mercadoria;
-//     var valor = formulario.valor;
-
-//     if (mercadoria.value == ""  || mercadoria == null)  {
-//         // verificar se o nome está vazio
-//         document.getElementById("erro-nome").innerHTML = "Nome não informado";
-
-//         // Deixa o input com o focus
-//         mercadoria.focus();
-//         // retorna a função e não olha as outras linhas
-//         return;
-//     }
-//     if (valor.value == "") {
-//         alert("Valor não informado");
-
-//         valor.focus();
-//         return;
-//     }
-// }
-
-
+var lista = []
+// VALIDAR SE TEM ERROS NO FORMULARIO 
 function validaçao() {
     var mercadoria = document.getElementById("mercadoria").value;
     var valor = document.getElementById("valor").value;
-
+    val = document.getElementById('formulario').value;
+    
+    
     if (mercadoria == "") {
         document.getElementById("mercadoria").focus();
         document.getElementById("erro-nome").innerHTML = "Nome não informado";
         return false;
-
+        
     } else if (valor == "") {
         document.getElementById("valor").focus();
         document.getElementById("erro-nome").innerHTML = "";
         document.getElementById("erro-valor").innerHTML = "Valor não informado";
         return false;
-
+        
     } else {
         document.getElementById("erro-nome").innerHTML = "";
         document.getElementById("erro-valor").innerHTML = "";
@@ -42,29 +24,55 @@ function validaçao() {
     }
 }
 
+// FUNÇÃO PARA BOTÃO ADICIONAR TRANSAÇAO 
 function validar() {
     if (validaçao() == true) {
-        var tipos = document.getElementById("tipos")
-        var opcao = tipos.options[selecao.selectedIndex].value;
+        var tipos = document.getElementById("tipos").value;
         var mercadoria = document.getElementById("mercadoria").value;
         var valor = document.getElementById("valor").value;
-
-        if (opcao == "compra") {
-            valor = valor.replace(".", "");
-            valor = valor.replace(",", ".");
-            resultado = resultado - parseFloat(valor);
-            document.getElementById("mercadoria").value = "";
-            document.getElementById("valor").value = "";
-
+        
+        if (tipos == "compras") {
+            valor = parseFloat(valor) * -1;
         } else {
-
-            valor = valor.replace(".", "");
-            valor = valor.replace(",", ".");
-            document.getElementById("mercadoria").value = "";
-            document.getElementById("valor").value = "";
+            valor = parseFloat(valor);
         }
+        lista.push({
+            mercadoria: mercadoria,
+            valor: valor,
+            tipos: tipos
+        })
+        console.log('lista', lista)
+        localStorage.setItem('lista', JSON.stringify(lista))
+        
+        document.getElementById("mercadoria").value = "";
+        document.getElementById("valor").value = "";
+        document.getElementById("tipos").value = "";
+    }
+    return false;
+}
+
+function listaDados() {
+    lista = JSON.parse(localStorage.getItem('lista'))
+    console.log("LISTA POPULADA ", lista);
+    
+    document.getElementById('lista').innerHTML == '';
+    
+    var total = 0;
+    for (let idx_aln in lista) {
+        console.log("NOME PRODUTO", lista[idx_aln].mercadoria);
+        document.getElementById('lista').innerHTML +=
+        `
+        <td colspan="3"><img src="./linha.png" class="linha-extrato"></td>'
+        <br>
+        <tr>
+        <td class="col-1">` + lista[idx_aln].tipos + `</td>
+        <td class="col-2">` + lista[idx_aln].mercadoria + `</td>
+        <td class="col-3">` + lista[idx_aln].valor + `</td>'
+        </tr>
+        `     
     }
 }
+// MASK
 
 function mascara() {
     var val = document.getElementById("valor");
@@ -77,13 +85,16 @@ function mascara() {
     val.value = res;
 }
 
+// CLEAN
 
 function limpar() {
-    var escolha = confirm("Confirme aqui!");
+    var escolha = confirm("CONFIRME AQUI!");
     if (escolha == true) {
         localStorage.clear();
         window.location.reload();
-    }
+        return;
+    } else(escolha == false)
+    alert('LIMPEZA CANCELADA!')
+    window.location.reload();
+    return;
 }
-
-localStorage.getItem 
