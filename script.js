abaa66c10a895023986fa10beb65e09888a3d0ca
giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-07-16 17:09:59 -0300

    finalização

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 var lista = []
+var aluno = "4863"
 // VALIDAR SE TEM ERROS NO FORMULARIO 
 function validaçao() {
     var mercadoria = document.getElementById("mercadoria").value;
     var valor = document.getElementById("valor").value;
     val = document.getElementById('formulario').value;
-    
-    
+
+
     if (mercadoria == "") {
         document.getElementById("mercadoria").focus();
         document.getElementById("erro-nome").innerHTML = "Nome não informado";
         return false;
-        
+
     } else if (valor == "") {
         document.getElementById("valor").focus();
         document.getElementById("erro-nome").innerHTML = "";
         document.getElementById("erro-valor").innerHTML = "Valor não informado";
         return false;
-        
+
     } else {
         document.getElementById("erro-nome").innerHTML = "";
         document.getElementById("erro-valor").innerHTML = "";
         return true;
     }
+
 }
 
 // FUNÇÃO PARA BOTÃO ADICIONAR TRANSAÇAO 
@@ -30,7 +32,7 @@ function validar() {
         var tipos = document.getElementById("tipos").value;
         var mercadoria = document.getElementById("mercadoria").value;
         var valor = document.getElementById("valor").value;
-        
+
         if (tipos == "compras") {
             valor = parseFloat(valor) * -1;
         } else {
@@ -39,38 +41,56 @@ function validar() {
         lista.push({
             mercadoria: mercadoria,
             valor: valor,
-            tipos: tipos
+            tipos: tipos,
+            total: total
         })
         console.log('lista', lista)
         localStorage.setItem('lista', JSON.stringify(lista))
-        
+
         document.getElementById("mercadoria").value = "";
         document.getElementById("valor").value = "";
         document.getElementById("tipos").value = "";
+        listaDados()
+        somaExtrato()
     }
     return false;
+
 }
+
 
 function listaDados() {
     lista = JSON.parse(localStorage.getItem('lista'))
     console.log("LISTA POPULADA ", lista);
-    
     document.getElementById('lista').innerHTML == '';
-    
+
+
     var total = 0;
     for (let idx_aln in lista) {
-        console.log("NOME PRODUTO", lista[idx_aln].mercadoria);
-        document.getElementById('lista').innerHTML +=
-        `
-        <td colspan="3"><img src="./linha.png" class="linha-extrato"></td>'
-        <br>
-        <tr>
-        <td class="col-1">` + lista[idx_aln].tipos + `</td>
-        <td class="col-2">` + lista[idx_aln].mercadoria + `</td>
-        <td class="col-3">` + lista[idx_aln].valor + `</td>'
-        </tr>
-        `     
+        total += parseFloat(lista[idx_aln].valor);
+        var sinais = "+"
+        if (lista[idx_aln].tipos == "compras") {
+            sinais = "-"
+        }
+        console.log("NOME PRODUTO", lista[idx_aln]);
+        document.getElementById('lista').innerHTML += `
+        <div class="linhas" id="linhas">
+        <span>` + sinais + `</span><p class="plinha">` + lista[idx_aln].mercadoria + `</p>
+           <p class="valorex">` + lista[idx_aln].valor + `</p>
+           </div>
+        `;
     }
+
+    document.querySelector('.resultado').innerHTML = total
+    if (total < 0) {
+
+        document.getElementById("resultFinal").innerHTML = "[PREJUIZO]";
+
+    } else {
+
+        document.getElementById("resultFinal").innerHTML = "[LUCRO]";
+
+    }
+
 }
 // MASK
 
@@ -95,6 +115,85 @@ function limpar() {
         return;
     } else(escolha == false)
     alert('LIMPEZA CANCELADA!')
-    window.location.reload();
+    event.preventDefault();
     return;
+}
+
+function salvar() {
+    lista = JSON.stringify(localStorage.getItem("lista"));
+    var datas = []
+
+    if (lista != null) {
+        fetch('https://api.airtable.com/v0/appRNtYLglpPhv2QD/Historico?fields=Aluno&fields=Json', {
+            headers: {
+                Authorization: 'Bearer key2CwkHb0CKumjuM'
+            }
+
+        }).then((resp) => {
+
+            return resp.json()
+        }).then((data) => {
+            datas = data.records
+
+
+            datas.forEach(element => {
+
+                if (element.fields.Aluno == "4863") {
+                    found = element.fields.Aluno.toString();
+                    id = element.id.toString();
+                }
+
+
+            })
+
+
+        })
+
+
+        if (found != "4863") {
+            fetch('https://api.airtable.com/v0/appRNtYLglpPhv2QD/Historico', {
+                method: "POST",
+                headers: {
+                    Authorization: 'Bearer key2CwkHb0CKumjuM',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    records: [{
+                            fields: {
+                                Aluno: '4863',
+                                Json: lista
+                            }
+
+                        }
+
+                    ]
+
+                })
+
+
+
+            })
+
+        } else {
+            fetch("https://api.airtable.com/v0/appRNtYLglpPhv2QD/Historico/" + id, {
+                method: "PUT",
+                headers: {
+                    Authorization: 'Bearer key2CwkHb0CKumjuM',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+
+                    records: [{
+                            fields: {
+                                Aluno: '4863',
+                                Json: lista
+                            }
+
+                        }
+
+                    ]
+                })
+            })
+        }
+    }
 }
